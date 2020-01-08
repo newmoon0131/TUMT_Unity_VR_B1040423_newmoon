c52a377281e89f8b428a2324fca6fd213283628a
newmoon0131 <55218701+newmoon0131@users.noreply.github.com>
2020-01-08 21:58:13 +0800

    完成VR

---
+++ Unity_VR_B1040423_newmoon/Assets/GameM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameM : MonoBehaviour
+{
+    public static bool haveK;
+
+    public void pick()
+    {
+
+    }
+    public void Npick(GameObject obj)
+    {
+        haveK = true;
+        Destroy(obj);
+    }
+    public void holde()
+    {
+
+    }
+}
---
+++ Unity_VR_B1040423_newmoon/Assets/TeleportP.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportP : MonoBehaviour
+{
+    public Animator ani;
+    public GameObject OBJ;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "HeadCollider"&&GameM.haveK)
+        {
+
+            OBJ.SetActive(true);
+        }
+        
+    }
+}
